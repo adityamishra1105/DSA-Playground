e840c5368dfbc27f099e0720eb72f80a07eed9fc
Aditya Mishra <adityaamishra11@gmail.com>
2025-02-08 11:48:46 +0530

    Added Linear Search implementation

--- Arrays/LinearSearch.java
+++ Arrays/LinearSearch.java
@@ -12,14 +12,14 @@ public class LinearSearch{
         int sum = 0;
         int[] arr = new int[size];
         System.out.println("Enter elements in the array: ");
-        for(int i = 0; i < size; i++){ // taking user input
+        for(int i = 0; i < size; i++){                   // taking user input
             arr[i] = sc.nextInt();
             sum += arr[i];
         }
         System.out.println("Enter the element you want to search for: ");
-        int target = sc.nextInt(); // elment to be searched
-        int ans = linearsearch(arr, target); //calling the function
-        if(ans != -1){ //printing the target element if found
+        int target = sc.nextInt();                      // elment to be searched
+        int ans = linearsearch(arr, target);            //calling the function
+        if(ans != -1){                                  //printing the target element if found
             System.out.println("Element was found at index: " + ans);
             System.out.println("The element found at index " + ans + " is " + arr[ans]);
         }
@@ -28,7 +28,7 @@ public class LinearSearch{
         }
         System.out.println("Sum of the element of the array is: "+sum);
     }
-    public static int linearsearch(int[] arr, int target){ //creating a method for linear search
+    public static int linearsearch(int[] arr, int target){        //creating a method for linear search
         if(arr.length == 0){
             return -1;
         }

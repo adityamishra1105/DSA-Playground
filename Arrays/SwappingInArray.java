88349c53f315bd50a3b9e396d4957df8f458faa7
Aditya Mishra <adityaamishra11@gmail.com>
2025-02-20 06:17:51 +0530

    Added Swapping in Array using a temporary variable

---
+++ Arrays/SwappingInArray.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class SwappingInArray{
+    public static void main(String[] args){
+        int[] arr = {1, 4, 6, 6, 96, 25, 642, 730};
+        swap(arr, 0, 3);
+        System.out.print(Arrays.toString(arr));
+    }
+    public static void swap(int[] arr, int index1, int index2){
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+}

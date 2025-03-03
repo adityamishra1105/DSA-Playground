c63d252cd13ab9f07f74e43b989b98220a1fa7f3
Aditya Mishra <adityaamishra11@gmail.com>
2025-03-03 21:50:39 +0530

    Added Searching folder

---
+++ Searching/LinearSearch.java
@@ -0,0 +1,39 @@
+package Searching;
+
+import java.util.Scanner;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of array: ");
+        int size = sc.nextInt();   
+        int[] arr = new int[size];
+        
+        System.out.print("Enter elements in the array: ");
+        for (int i = 0; i < size; i++) { // taking user input
+            arr[i] = sc.nextInt();
+        }
+        
+        System.out.print("Enter the element you want to search for: ");
+        int target = sc.nextInt(); // element to be searched
+
+        int ans = linearsearch(arr, target); // calling the function
+
+        if (ans != -1) { // printing the target element if found
+            System.out.println("Element was found at index: " + ans);
+            System.out.print("The element found at index " + ans + " is " + arr[ans]);
+        } else {
+            System.out.print("Element was not found in the array.");
+        }
+        sc.close();
+    }
+
+    public static int linearsearch(int[] arr, int target) { // creating a method for linear search
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] == target) {
+                return index; // return the index immediately when found
+            }
+        }
+        return -1; // return -1 if not found
+    }
+}

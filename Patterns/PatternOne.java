0ecf00ee8768b758972b2bb3da9bb77f47eb06ce
Aditya Mishra <adityaamishra11@gmail.com>
2025-03-30 21:30:32 +0530

    Print a Square

--- Patterns/PatternOne.java
+++ Patterns/PatternOne.java
@@ -18,6 +18,10 @@ public class PatternOne{
 
         sc.close();
     }
+
+
+    // function to prin the pattern 
+    
     static void printPattern(int n, int m){
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {

b3f1a1f6a44bf73ad1616952ab36d8d0d39f4dd3
Aditya Mishra <adityaamishra11@gmail.com>
2025-03-31 01:03:58 +0530

    Program to print a Pyramid

---
+++ Patterns/PatternFour.java
@@ -0,0 +1,29 @@
+// Program to print a pyramid
+
+
+package Patterns;
+
+import java.util.Scanner;
+
+public class PatternFour {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int n = sc.nextInt();
+        printPattern(n);
+
+        sc.close();
+    }
+
+    static void printPattern(int n){
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

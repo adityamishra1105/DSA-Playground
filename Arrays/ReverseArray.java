cde8dbccd90214bb0c20e2725cfd881bda747cbb
Aditya Mishra <adityaamishra11@gmail.com>
2025-04-01 20:39:19 +0530

    Updated array reversal code

--- Arrays/ReverseArray.java
+++ Arrays/ReverseArray.java
@@ -1,14 +1,29 @@
 import java.util.Arrays;
+import java.util.Scanner;
 public class ReverseArray{
     /**
      * @param args
      */
     public static void main(String[] args){
-        int[] arr = {1, 4, 6, 6, 96, 25, 642, 730};
-        swap(arr, 0, 3);
-        System.out.println(Arrays.toString(arr));
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length of the Array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements inside the Array: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Enter index one: ");
+        int index1 = sc.nextInt();
+        System.out.print("Enter index two: ");
+        int index2 = sc.nextInt();
+
+        swap(arr, index1, index2);
+        System.out.println("Array before reversal" + Arrays.toString(arr));
         reverse(arr);
-        System.out.print(Arrays.toString(arr));
+        System.out.print("Array before reversal" + Arrays.toString(arr));
+
+        sc.close();
         
     }
     static void reverse(int[] arr){

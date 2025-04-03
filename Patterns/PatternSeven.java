b1e042f5c4e4c3e0848bdf4cfd3d0b431f90b43e
Aditya Mishra <adityaamishra11@gmail.com>
2025-04-03 22:16:56 +0530

    Triangle pattern

---
+++ Patterns/PatternSeven.java
@@ -0,0 +1,22 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class PatternSeven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        printPattern(n);
+        sc.close();
+    }
+
+    static void printPattern(int n){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}

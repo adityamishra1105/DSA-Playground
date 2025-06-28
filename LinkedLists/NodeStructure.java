2c31e7475395359b0c5562fdcc91e620d4f38160
Aditya Mishra <adityaamishra11@gmail.com>
2025-06-28 23:41:01 +0530

    Updated Node Structure

--- LinkedLists/NodeStructure.java
+++ LinkedLists/NodeStructure.java
@@ -1,14 +1,13 @@
 /* A linked list is a linear data structure in which elements (nodes) are stored in sequence,
  * each containing a data part and a reference (or link) to the next node in the sequence.
  */
-
 // Each node in a linkedlist contains: 
 // Data (value of the node)
 // Pointer (address of the next node)
 
 class Node{
     int data;
-    Node next; // Pointer
+    Node next; // Pointer to the next variable
 
     Node(int data){
         this.data = data;

f6936305f35352436ec881796d940f60a8731211
Aditya Mishra <adityaamishra11@gmail.com>
2025-07-19 00:42:35 +0530

    updated code

--- Arrays/MaxValueInArray.java
+++ Arrays/MaxValueInArray.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 public class MaxValueInArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = {1, 4, 7, 3, 87, 35};
+        int[] arr = {1, 4, 7, 3, 87, 35};       //you can also take user input if you want to
         int start = 1;
         int end = 3;
         System.out.println("Max in full array: " + max(arr)); // Prints max in the full array
         System.out.println("Max in range (" + start + ", " + end + "): " + maxrange(arr, start, end)); // Prints max in the range
     
-        sc.close();
+        sc.close();      //adapt a habit of closing the scanner as it is a good practice
     }
+
     // Method to find the max value in the whole array
     public static int max(int[] arr) {
         int max = arr[0]; // assuming the first element is the max
@@ -20,6 +21,7 @@ public class MaxValueInArray {
         }
         return max; // Return the max val
     }
+    
     // Method to find the max value in a specific range of indices in the array
     public static int maxrange(int[] arr, int start, int end) {
         int max = arr[start]; // Set the initial max to the start index
